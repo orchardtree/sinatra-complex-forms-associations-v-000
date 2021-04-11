1623fbcee186100f11d48fcd8a0550aa33b09378
Jesse Wenger <wengerj@gmail.com>
2021-04-11 16:15:30 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,41 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if params["name"].empty?
+      @owner = Owner.find(params[:owner])
+      @owner.pets << @pet
+    else
+      @owner = Owner.create(:name => params[:name])
+      @owner.pets << @pet
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
+  
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
   patch '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    if params["owner"]["name"].empty?
+      @pet.update(params["pet"])
+    else
+      @owner = Owner.create(params[:owner])
+      @owner.pets << @pet
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
